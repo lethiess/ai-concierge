b5f5e4648a2d4a8ebca7860863abf987dcd9a59d
Leonard Thießen <2551258+lethiess@users.noreply.github.com>
2025-09-12 23:21:10 +0200

    chore: added pre-commit hook

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
-from agents import Agent, function_tool, WebSearchTool, FileSearchTool, set_default_openai_key
-from agents.extensions.handoff_prompt import prompt_with_handoff_instructions
+from agents import set_default_openai_key
 
 set_default_openai_key(os.getenv("OPENAI_API_KEY"))
+
 
 def main():
     print("Hello from concierce!")
